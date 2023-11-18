0b4ffee75aef7ecaa58c2a09001b6134409573dc
webdiller <eugenefromrus@gmail.com>
2023-11-18 21:43:43 +1000

    Initial commit from Create Next App

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+interface ComponentProps {}
+
+const AboutPage: FC<ComponentProps> = () => {
+  return <div>AboutPage</div>;
+};
+
+export default AboutPage;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+interface ComponentProps {}
+
+const HomePage: FC<ComponentProps> = () => {
+  return <div>HomePage</div>;
+};
+
+export default HomePage;
